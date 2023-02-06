e546bb3ec40183db07735aad7dba9fe0e11635ca
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2023-02-05 19:31:36 -0500

    Change query return types

--- src/main/java/com/mockcompany/webapp/data/ProductItemRepository.java
+++ src/main/java/com/mockcompany/webapp/data/ProductItemRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * This Spring pattern is Java/Spring magic.  At runtime, spring will generate an implementation of this class based on
  * the name/arguments of the method signatures defined in the interface.  See this link for details on doing data access:
@@ -19,9 +21,9 @@ import org.springframework.stereotype.Repository;
 public interface ProductItemRepository extends CrudRepository<ProductItem, Long> {
 
     @Query(value = "SELECT p FROM ProductItem p WHERE LOWER(p.name) LIKE %?1% or LOWER(p.description) LIKE %?1%")
-    Iterable<ProductItem> findLikeSearch(String query);
+    List<ProductItem> findLikeSearch(String query);
 
     @Query(value = "SELECT p FROM ProductItem p WHERE LOWER(p.name)=?1 or LOWER(p.description)=?1")
-    Iterable<ProductItem> findLikeSearchExact(String query);
+    List<ProductItem> findLikeSearchExact(String query);
 
 }

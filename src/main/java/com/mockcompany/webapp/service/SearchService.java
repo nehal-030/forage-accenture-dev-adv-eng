4d66ee158031e147f1631d5cd9eb04381f06eb57
William Couturiaux <37006112+wcouturiaux@users.noreply.github.com>
2023-02-22 23:17:06 -0500

    Add list import

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -5,7 +5,7 @@ import com.mockcompany.webapp.model.ProductItem;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 
 @Service
 public class SearchService {
